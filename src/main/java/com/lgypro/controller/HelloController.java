9c54540ea208e7973a4c09a41a8bb176e9e2dffe
lgypro <kaixin.lgx@gmail.com>
2023-07-07 16:47:17 +0800

    add /hello endpoint

--- src/main/java/com/lgypro/controller/HelloController.java
+++ src/main/java/com/lgypro/controller/HelloController.java
@@ -9,4 +9,9 @@ public class HelloController {
     public String index() {
         return "Greetings from Spring Boot!";
     }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello!";
+    }
 }
